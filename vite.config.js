5fd318168ce95887695a333ad67e36b6942850f5
simplearyan <aryanphone00620@gmail.com>
2026-04-13 22:00:16 +0530

    chore: setup github pages workflow and vite config

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Required for GitHub Pages deployment. It maps assets to /chart-studio/ instead of /
+  base: '/chart-studio/'
+});
